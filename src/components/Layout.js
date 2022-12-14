3c4d8f32bacd60b0081b6e6ee6824ae3c41c4389
ChristinaDian <krisdian2001@gmail.com>
2022-12-14 12:39:48 +0200

    Added files and folders for components

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import { Footer } from "Footer";
+import { Header } from "Header";
+import { Main } from "Main";
+
+export function Layout() {
+
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
