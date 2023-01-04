97eeed5b3cf87b62482f38f3edf424015d0da6f8
elizgunay <82233161+elizgunay@users.noreply.github.com>
2023-01-05 01:45:18 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-import { Box } from "@mui/material";
-import { Stack } from "@mui/system";
-import Feed from "./HomePage/Feed"
-import Rightbar from "./HomePage/Rightbar";
-import Sidebar from "./HomePage/Sidebar";
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { TopicsList } from './components/topics/TopicsList';
-import { TopicForm } from './components/topics/TopicForm';
-import { TopicDetails } from './components/topics/TopicDetails';
+import "./App.css";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import QuestionCreate from './Components/Questions/QuestionCreate';
+import QuestionEdit from './Components/Questions/QuestionEdit';
+import QuestionList from './Components/Questions/QuestionList';
 
 
 function App() {
 
   return (
-    
-    <Box>
-      <header>
-        <div class="center">
-        <p>============================</p>
-        <p>Hi, this space is for header</p>
-        <p>============================</p>
-        </div>
-      </header>
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-      </Stack>
-      <footer>
-      <div class="center">
-      <p>============================</p>
-      <p>Hi, this space is for footer</p>
-      <p>============================</p>
-      </div>
-      </footer>
-    
     <div className="App">
-      <Routes>
-        <Route path="/topics-list" element={<TopicsList />} />
-        <Route path="/topic/create" element={<TopicForm />} />
-        <Route path="/topic/edit/:id" element={<TopicForm />} />
-        <Route path="/topic/:id" element={<TopicDetails />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<QuestionList />}></Route>
+          <Route path="/question/create" element={<QuestionCreate />}></Route>
+          <Route path="/question/edit/:qid" element={<QuestionEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-    </Box>
+   
   );
 }
 
