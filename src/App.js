fdbf4471908801c4626d01d538df91a9e5e186ea
elizgunay <82233161+elizgunay@users.noreply.github.com>
2023-01-09 22:52:47 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { Box } from "@mui/material";
+import { Stack } from "@mui/system";
+import Feed from "./HomePage/Feed"
+import Rightbar from "./HomePage/Rightbar";
+import Sidebar from "./HomePage/Sidebar";
+
 import "./App.css";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import QuestionCreate from './Components/Questions/QuestionCreate';
@@ -8,16 +14,36 @@ import QuestionList from './Components/Questions/QuestionList';
 function App() {
 
   return (
+    <Box>
+    <header>
+      <div class="center">
+        <p>============================</p>
+        <p>Hi, this space is for header</p>
+        <p>============================</p>
+      </div>
+    </header>
+    <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Sidebar />
     <div className="App">
-      <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Feed />} />
+
           <Route path="/" element={<QuestionList />}></Route>
           <Route path="/question/create" element={<QuestionCreate />}></Route>
           <Route path="/question/edit/:qid" element={<QuestionEdit />}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
-   
+    <Rightbar />
+      </Stack>
+      <footer>
+        <div class="center">
+          <p>============================</p>
+          <p>Hi, this space is for footer</p>
+          <p>============================</p>
+        </div>
+      </footer>
+
+    </Box>
   );
 }
 
